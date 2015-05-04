ded58e330b091e8c228a6d8bd491538819620397
dszczepanska <dagna.szczepanska@gmail.com>
2015-05-04 21:57:15 +0200

    Widok dla przywracania hasła

---
+++ OpenPKW-Mobile/Services/IRemindService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPKW_Mobile.Services
+{
+    interface IRemindService
+    {
+        /// <summary>
+        /// Zdarzenie informujące o poprawej zmianie hasłą
+        /// </summary>
+        event Action<string> RemindCompleted;
+
+        /// <summary>
+        /// Zdarzenie informujące o błedzie podczas zmiany hasła
+        /// </summary>
+        event Action<string> RemindRejected;
+
+        /// <summary>
+        /// Rozpoczęcie procedury odzyskiwanie hasła.
+        /// Procedura jest wykonywana w tle, a jej wynik zgłaszany poprzez zdarzenia.
+        /// </summary>
+        /// <param name="userName">Nazwa użytkownika.</param>
+        /// <param name="userEmail">E-mail użytkownika.</param>        
+        void RemindLogin(string userName, string userEmail);
+    }
+}
---
+++ OpenPKW-Mobile/Services/RemindService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPKW_Mobile.Services
+{
+    /// <summary>
+    /// usługa zmiany hasła użytkownika
+    /// </summary>
+    class RemindService : IRemindService
+    {
+        /// <summary>
+        /// Zestaw danych przekazywanych pomiędzy wątkami.
+        /// </summary>
+        struct WorkerData
+        {
+            public string UserName;
+            public string UserPassword;
+            public IAuthenticationProvider AuthenticationProvider;
+        }
+    }
+}
