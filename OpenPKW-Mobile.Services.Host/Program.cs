a783e201dacc0a6fec2646bf5f09ec528524a967
emron <openpkw@emron.pl>
2015-05-12 00:52:50 +0200

    Utworzenie projektu z serwisami testowymi oraz projektu hosta serwisów

---
+++ OpenPKW-Mobile.Services.Host/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPKW_Mobile.Services.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
